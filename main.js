39595f9a3a4483f28bd17aa56ffcb96ecfca6f00
Jana <23-101109@students.eui.edu.eg>
2025-12-12 22:56:38 +0200

    GSAP car entering landing screen animation

--- main.js
+++ main.js
@@ -4,34 +4,69 @@ const mainHeadline = document.querySelector(".main-headline");
 const split = new SplitText(mainHeadline, { type: "chars" });
 const chars = split.chars;
 
-// GROUPS
-const group1 = chars.filter((_, i) => i % 2 === 0); // even index letters
-const group2 = chars.filter((_, i) => i % 2 !== 0); // odd index letters
+
+const group1 = chars.filter((_, i) => i % 2 === 0);
+const group2 = chars.filter((_, i) => i % 2 !== 0);
 
 const tl = gsap.timeline();
 
-// GROUP 1 — start mid, go to top
+
 tl.from(group1, {
-  y: "50vh",     // middle of viewport
+  y: "50vh",   
   opacity: 0,
+  delay: 0.25,
   ease: "power3.out",
   stagger: 0.08,
 });
 
-// GROUP 2 — start bottom → STOP MID → GO TOP
 tl.from(group2, {
-  y: "100vh",    // from very bottom
+  y: "100vh",    
   opacity: 0,
   ease: "power3.out",
   stagger: 0.1,
-}, "-=0.4")      // overlap a bit with group1
+}, "-=0.4")     
 
-// THEN MOVE TO FINAL POSITION
 tl.to(group2, {
-  y: 0,          // final aligned position
+  y: 0,       
   duration: 0.6,
   ease: "power3.out",
   stagger: 0.1,
+});
+
+
+// landing screen car model
+const modelViewer = document.querySelector('#heroCar');
+
+modelViewer.addEventListener('load', () => {
+  // 1. Target the material and set to black
+  const PAINT = modelViewer.model.getMaterialByName('PAINT');
+  if (PAINT) {
+    // [r, g, b, a] normalized 0-1
+    PAINT.pbrMetallicRoughness.setBaseColorFactor([0, 0, 0, 1]); 
+  }
+
+  // 2. Reveal the model now that it's ready
+  modelViewer.dismissPoster();
+  
+  // 3. Fade in the component
+  modelViewer.style.opacity = 1;
+});
+
+gsap.from("#heroCar", {
+  x: "-120%",
+  opacity: 0,
+  filter: "blur(10px)",
+  duration: 1.8,
+  delay: 1,
+  ease: "power3.out"
+});
+
+gsap.to("#heroCar", {
+  x: 0,
+  opacity: 1,
+  duration: 1.6,
+  delay: 2,
+  ease: "power3.out"
 });
 
 
